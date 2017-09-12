de612d626874aff5354835e06c148f7fde097146
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-09-12 07:56:19 +0200

    feat: Added vector operation examples

--- drills.py
+++ drills.py
@@ -7,6 +7,7 @@
 
     Repetative practice drills
 """
+import time
 import numpy as np
 
 
@@ -93,11 +94,43 @@ def softmax(x):
     """
     x_exp = np.exp(x)
 
-    x_sum = np.sum(x_exp, axis = 1, keepdims = True)
+    x_sum = np.sum(x_exp, axis=1, keepdims=True)
 
     xs = x_exp / x_sum
 
     return xs
+
+
+def vector_operations():
+    """Practice typical vector operations
+
+    """
+    x1 = [9, 2, 5, 0, 0, 7, 5, 0, 0, 0, 9, 2, 5, 0, 0]
+    x2 = [9, 2, 2, 9, 0, 9, 2, 5, 0, 0, 9, 2, 5, 0, 0]
+
+    # VECTORIZED DOT PRODUCT OF VECTORS
+    tic = time.process_time()
+    dot = np.dot(x1, x2)
+    toc = time.process_time()
+    print("dot = " + str(dot) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
+
+    # VECTORIZED OUTER PRODUCT
+    tic = time.process_time()
+    outer = np.outer(x1, x2)
+    toc = time.process_time()
+    print("outer = " + str(outer) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
+
+    # VECTORIZED ELEMENTWISE MULTIPLICATION
+    tic = time.process_time()
+    mul = np.multiply(x1, x2)
+    toc = time.process_time()
+    print("elementwise multiplication = " + str(mul) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
+
+    # VECTORIZED GENERAL DOT PRODUCT
+    tic = time.process_time()
+    dot = np.dot(W, x1)
+    toc = time.process_time()
+    print("gdot = " + str(dot) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
 
 
 def sigmoid_test():
