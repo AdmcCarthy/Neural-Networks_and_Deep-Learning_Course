896da740c249229aca3310bc02a82b666ac789cc
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-09-24 12:21:10 +0200

    feat: Worked on image2vec as practice

--- practice.py
+++ practice.py
@@ -30,3 +30,20 @@ def sigmoid_prime(x):
     ds = sigmoid(x) * (1 - sigmoid(x))
 
     return ds
+
+
+def image2vec(x):
+    """Convert an image to a vector.
+
+    Reshape the image array into a one
+    dimensional vector.
+
+    Args:
+        x: A numpy array of shape (length, height, depth).
+    Returns:
+        xv: A vector of shape (length*height*depth, 1).
+    """
+
+    xv = x.reshape(x.shape[0] * x.shape[1] * x.shape[2], 1)
+
+    return xv
